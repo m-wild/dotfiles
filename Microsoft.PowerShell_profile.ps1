27ef5d9256c64b85fb3571ef74e4e1aef31becf9
tehmantra <michael@mwild.me>
2015-05-31 12:45:59 +1200

    initial commit

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,18 @@
+## powershell profile
+## michael@mwild.me
+
+# module
+Import-Module PsGet
+Import-Module PsReadLine
+
+# alias
+New-Alias ll ls
+New-Alias open start
+New-Alias dig "C:\Program Files\BIND\dig.exe"
+New-Alias lame "C:\Program Files\lame\lame.exe"
+New-Alias npp "C:\Program Files (x86)\Notepad++\notepad++.exe"
+New-Alias putty "C:\Program Files (x86)\PuTTY\putty.exe"
+
+# home
+$h = "G:\downloads"
+cd $h
