e9c74efa5e837fe1229184b31ff090df5ef53fcc
tehmantra <michael@mwild.me>
2015-05-31 22:09:56 +1200

    added func 'which'

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -13,6 +13,10 @@ New-Alias lame "C:\Program Files\lame\lame.exe"
 New-Alias npp "C:\Program Files (x86)\Notepad++\notepad++.exe"
 New-Alias putty "C:\Program Files (x86)\PuTTY\putty.exe"
 
+# func
+function which ($com) { (Get-Command -All $com).Definition }
+
+
 # home
 $h = "G:\downloads"
 cd $h
