e00f34a1172f66c1cf0dfebf060a15e7349288b8
Michael Wildman <michael@mwild.me>
2015-08-01 21:26:40 +1200

    Updates for Windows 10

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,23 +1,18 @@
 ## powershell profile
 ## michael@mwild.me
 
-# module
-Import-Module PsGet
-Import-Module PsReadLine
-
 # alias
-New-Alias ll ls
 New-Alias open start
 New-Alias grep Select-String
 New-Alias gch Get-ChildItem
 New-Alias dig "C:\Program Files\BIND\dig.exe"
 New-Alias lame "C:\Program Files\lame\lame.exe"
 New-Alias npp "C:\Program Files (x86)\Notepad++\notepad++.exe"
-New-Alias vi npp
 New-Alias putty "C:\Program Files (x86)\PuTTY\putty.exe"
 New-Alias ssh putty
 
 # func
+function ll { ls -force }
 function which ($com) { (Get-Command -All $com).Definition }
 function tail ($file) { Get-Content $file -Tail 10 -Wait }
 
