8a14a4fe7953304b3c55edadcdb2ba7016ce6cb8
Michael Wildman <michael@mwild.me>
2015-12-23 21:55:56 +1300

    style update

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,25 +1,43 @@
 ## powershell profile
 ## michael@mwild.me
 
-# alias and functions
-New-Alias open start
-New-Alias grep Select-String
-New-Alias gch Get-ChildItem
-New-Alias dig "C:\Program Files\ISC BIND 9\bin\dig.exe"
-New-Alias npp "C:\Program Files (x86)\Notepad++\notepad++.exe"
-New-Alias putty "C:\Program Files (x86)\PuTTY\putty.exe"
-New-Alias ssh putty
-New-Alias plink 'C:\Program Files (x86)\PuTTY\plink.exe'
-New-Alias less "C:\Program Files (x86)\GnuWin32\bin\less.exe"
-New-Alias youtube-dl "C:\Program Files (x86)\youtube-dl\youtube-dl.exe"
-New-Alias ffmpeg "C:\Program Files\ffmpeg\bin\ffmpeg.exe"
-
-function which ($com) { (Get-Command -All $com).Definition }
-function tail ([switch]$f,$file) {  Get-Content $file -Tail 10 -Wait }
+#### aliases
+# powershell native
+new-alias open start
+new-alias grep Select-String
+new-alias gch Get-ChildItem
 function ll { ls -Force }
+
+# emulation of linux "which"
+function which ($com) { (Get-Command -All $com).Definition }
+
+# alias of linux "tail" with "-f" switch implemented
+function tail ([switch]$f,$file) {  if ($f) { Get-Content $file -Tail 10 -Wait } else { Get-Content $file -Tail 10 } }
+
+# useful for cleaning up after nodejs for example
 function reset-color { [Console]::ResetColor() }
 
-# home
+# isc bind
+new-alias dig "C:\Program Files\ISC BIND 9\bin\dig.exe"
+
+# editors
+new-alias npp "C:\Program Files (x86)\Notepad++\notepad++.exe"
+new-alias vim "C:\Program Files (x86)\vim\vim74\vim.exe"
+new-alias vi vim
+new-alias less "C:\Program Files (x86)\GnuWin32\bin\less.exe"
+
+# ssh (putty)
+new-alias putty "C:\Program Files (x86)\PuTTY\putty.exe"
+new-alias ssh putty
+new-alias plink 'C:\Program Files (x86)\PuTTY\plink.exe'
+
+# media
+new-alias youtube-dl "C:\Program Files (x86)\youtube-dl\youtube-dl.exe"
+new-alias ffmpeg "C:\Program Files\ffmpeg\bin\ffmpeg.exe"
+
+####
+# set a new "home" directory
 $h = "G:\downloads"
 cd $h
+
 
