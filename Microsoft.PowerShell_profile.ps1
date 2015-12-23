95d56b5ba6191caa9c2f0ab2754e46f723174f94
Michael Wildman <michael.wildman@callplus.co.nz>
2015-12-23 20:25:00 +1300

    remove some unused alises

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,32 +1,25 @@
 ## powershell profile
 ## michael@mwild.me
 
-# module
-Import-Module PsGet
-Import-Module PsReadLine
+# alias and functions
 
-# alias
-New-Alias ll ls
 New-Alias open start
 New-Alias grep Select-String
 New-Alias gch Get-ChildItem
 New-Alias dig "C:\Program Files\ISC BIND 9\bin\dig.exe"
-#New-Alias lame "C:\Program Files\lame\lame.exe"
 New-Alias npp "C:\Program Files (x86)\Notepad++\notepad++.exe"
-New-Alias vi npp
 New-Alias putty "C:\Program Files (x86)\PuTTY\putty.exe"
 New-Alias ssh putty
-New-Alias svcutil "C:\Program Files (x86)\Microsoft SDKs\Windows\v8.1A\bin\NETFX 4.5.1 Tools\SvcUtil.exe"
-New-Alias installutil "C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installutil.exe"
+New-Alias plink "C:\Program Files (x86)\PuTTY\plink.exe"
+#New-Alias less "C:\Program Files (x86)\GnuWin32\bin\less.exe"
+New-Alias tf "C:\Program Files (x86)\Microsoft Visual Studio 14.0\Common7\IDE\TF.exe"
 
-
-
-
-# func
 function which ($com) { (Get-Command -All $com).Definition }
-function tail ($file) { Get-Content $file -Tail 10 -Wait }
-
+function tail ([switch]$f,$file) {  Get-Content $file -Tail 10 -Wait }
+function ll { ls -Force }
+function reset-color { [Console]::ResetColor() }
 
 # home
-$h = "D:\downloads"
+$h = "D:\michael.wildman"
 cd $h
+
