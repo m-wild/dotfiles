0774c9401ac94af74c06f847829b16e86b93693c
Michael Wildman <michael@mwild.me>
2015-12-23 20:34:32 +1300

    new aliases: ffmpeg,ffprobe,youtube-dl

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -10,6 +10,12 @@ New-Alias lame "C:\Program Files\lame\lame.exe"
 New-Alias npp "C:\Program Files (x86)\Notepad++\notepad++.exe"
 New-Alias putty "C:\Program Files (x86)\PuTTY\putty.exe"
 New-Alias ssh putty
+New-Alias plink 'C:\Program Files (x86)\PuTTY\plink.exe'
+New-Alias less "C:\Program Files (x86)\GnuWin32\bin\less.exe"
+New-Alias youtube-dl "C:\Program Files (x86)\youtube-dl\youtube-dl.exe"
+New-Alias ffmpeg "C:\Program Files\ffmpeg\bin\ffmpeg.exe"
+New-Alias ffprobe "C:\Program Files\ffmpeg\bin\ffprobe.exe"
+
 
 # func
 function ll { ls -force }
