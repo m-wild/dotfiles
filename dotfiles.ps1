eb10962ca5201105db90c10a66ef5ed6b7fe529e
Michael Wildman <michael@mwild.me>
2020-06-03 23:32:21 +1200

    clean-up dotfiles script

--- dotfiles.ps1
+++ dotfiles.ps1
@@ -3,63 +3,53 @@
 
 [CmdletBinding()]
 param(
-    [string][validateset("build","clean")] $action,
-	[string] $path,
-    [switch] $help
+    [String][ValidateSet('Build','Clean')] $Action,
+	[String] $Path,
+    [Switch] $Help
 )
 process {
-    $ErrorActionPreference = "stop"
+    $ErrorActionPreference = 'Stop'
 
-    function Test-IsDirectory ($Path) { return (Get-Item -Path $Path).PSIsContainer }
-
-    write-host "-.. --- - ..-. .. .-.. . ..." -foregroundColor cyan # dotfiles morse code
-    if (($Help) -or !($action)) {
-        write-host "creates symlinks for your dotfiles"
-        write-host "usage: dotfiles [-action] <action> [-path <path_to_data_directory>]"
-        write-host ""
-        write-host "  -action   build (create symlinks), clean (remove symlinks)" 
-        write-host "  -path     path to data directory (default: .\data)"
-        write-host ""
+    Write-Host "-.. --- - ..-. .. .-.. . ..." -ForegroundColor cyan # dotfiles morse code
+    if (($Help) -or !($Action)) {
+        Write-Host 'creates symlinks for your dotfiles'
+        Write-Host 'usage: dotfiles [-action] <action> [-path <path_to_data_directory>]'
+        Write-Host ''
+        Write-Host '  -Action   Build (create symlinks), Clean (remove symlinks)'
+        Write-Host '  -Path     Path to data directory (default: .\data)'
+        Write-Host ''
         return
     }
 
-    if (!$path) { $path = (join-path $PSScriptRoot "data") }
+    if (!$Path) { $Path = (Join-Path $PSScriptRoot 'data') }
 
-    $config = get-content -path (join-path $path "dotfiles.json") -raw | convertfrom-json
+    $config = Get-Content -Path (Join-Path $Path 'dotfiles.json') -Raw | ConvertFrom-Json
 
-    # --- 1. symlinks
-    foreach ($dotfile in $config.symlink | where enabled) {
-        write-host "`n>> $($dotfile.name)"
+    foreach ($dotfile in $config.symlinks | Where-Object enabled) {
+        Write-Host "`n• $($dotfile.name)"
 
         $dotfile.link = [System.Environment]::ExpandEnvironmentVariables($dotfile.link)
         $dotfile.link = $ExecutionContext.InvokeCommand.ExpandString($dotfile.link)
-    
-        if ($action -eq "clean") {
-            if (!(test-path $dotfile.link)) {
-                write-host "$($dotfile.link) already clean"
-            } else {
-                write-host "removing link $($dotfile.link)"
 
-                # this is a quirk of trying to delete directory symlinks in powershell
-                if (Test-IsDirectory $dotfile.link) {
-                    cmd.exe /c rmdir $dotfile.link
-                } else {
-                    remove-item -Path $dotfile.link
-                }
-            }
-        } 
-        elseif ($action -eq "build") {
-            $dotfile_linkDir = split-path $dotfile.link
-            if (-not (test-path $dotfile_linkDir)) { mkdir $dotfile_linkDir | out-null }
-
-            $dotfile.target = Join-Path $path $dotfile.target
-            
-            # we have to tell mklink if the target is a directory
-            if (Test-IsDirectory $dotfile.target) { 
-                cmd.exe /c mklink /d $dotfile.link $dotfile.target
+        if ($Action -eq 'Clean') {
+            if (!(Test-Path $dotfile.link)) {
+                Write-Host "  $($dotfile.link) already clean"
             } else {
-                cmd.exe /c mklink $dotfile.link $dotfile.target
+                Write-Host "  Removing $($dotfile.link)"
+
+                Remove-Item -Path $dotfile.link
             }
+        } elseif ($Action -eq 'Build') {
+            $dotfileLinkDir = Split-Path $dotfile.link
+            if (-not (Test-Path $dotfileLinkDir)) { 
+                New-Item -ItemType Directory -Path $dotfileLinkDir | Out-Null 
+            }
+
+            $dotfile.target = Join-Path $Path $dotfile.target
+
+            Write-Host "  $($dotfile.link)`n  -> $($dotfile.target)"
+
+            New-Item -ItemType SymbolicLink -Path $dotfile.link -Target $dotfile.target | Out-Null
         }
     }
 }
